edeb4610f24588583f4a69f988669a5f766cd94c
YigitEmirUzun <uzunygt52@gmail.com>
2025-06-09 20:10:01 +0300

    Update Menu.cs

--- PetGameApp/Menu.cs
+++ PetGameApp/Menu.cs
@@ -1,15 +1,15 @@
-using System;
-
 public static class Menu
 {
     public static void ShowMainMenu()
     {
         Console.WriteLine("\n--- MAIN MENU ---");
-        Console.WriteLine("1 - Steal Pets");
-        Console.WriteLine("2 - View Stolen Pets");
-        Console.WriteLine("3 - Feed a Pet");
-        Console.WriteLine("4 - Exit");
-        Console.WriteLine("5 - Credits");
+        Console.WriteLine("1. Steal Pet");
+        Console.WriteLine("2. Show Stolen Pets");
+        Console.WriteLine("3. Feed Pet");
+        Console.WriteLine("4. Play With Pet");
+        Console.WriteLine("5. Put Pet To Sleep");
+        Console.WriteLine("6. Credits");
+        Console.WriteLine("7. Exit");
         Console.Write("Choose an option: ");
     }
 
