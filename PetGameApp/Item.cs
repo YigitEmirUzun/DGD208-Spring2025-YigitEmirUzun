34758804fd87ea90a573e6c036f65f2b0c6fdf4d
YigitEmirUzun <uzunygt52@gmail.com>
2025-06-09 20:09:13 +0300

    Update Item.cs

--- PetGameApp/Item.cs
+++ PetGameApp/Item.cs
@@ -3,18 +3,20 @@ using System.Collections.Generic;
 public class Item
 {
     public string Name { get; set; }
-    public int NutritionValue { get; set; }
-    public int HappinessBoost { get; set; }
-    public int HappinessPenalty { get; set; }
+    public int NutritionValue { get; set; }     
+    public int HappinessBoost { get; set; }     
+    public int HappinessPenalty { get; set; }    
     public List<string> LikedByPets { get; set; }
+    public ItemType Type { get; set; }       
 
-    public Item(string name, int nutrition, int happyBoost, int happyPenalty, List<string> likedByPets)
+    public Item(string name, int nutrition, int happyBoost, int happyPenalty, List<string> likedByPets, ItemType type)
     {
         Name = name;
         NutritionValue = nutrition;
         HappinessBoost = happyBoost;
         HappinessPenalty = happyPenalty;
         LikedByPets = likedByPets;
+        Type = type;
     }
 
     public bool LikesPet(string petName)
